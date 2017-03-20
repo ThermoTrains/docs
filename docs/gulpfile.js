1abf29c9ca54cfb9be42f566e03ae96f6b301804
Sebastian Häni <haeni.sebastian@gmail.com>
2017-03-20 21:38:39 +0100

    change of code style

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -14,66 +14,66 @@ const moment = require('moment');
 const pkg = JSON.parse(fs.readFileSync('./package.json'));
 
 gulp.task('default', function () {
-    runSequence('clean', ['nunjucks', 'js', 'styles', 'images']);
+  runSequence('clean', ['nunjucks', 'js', 'styles', 'images']);
 });
 
 gulp.task('clean', function () {
-    return del(['build/**/*']);
+  return del(['build/**/*']);
 });
 
 gulp.task('nunjucks', function () {
-    return gulp.src('index.html')
-        .pipe(nunjucksRender({
-            data: {
-                documentVersion: pkg.version,
-                buildDate: moment().format('DD.MM.YYYY')
-            }
-        }))
-        .on('error', function (error) {
-            gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
-            this.emit('end');
-        })
-        .pipe(gulp.dest('./build'));
+  return gulp.src('index.html')
+    .pipe(nunjucksRender({
+      data: {
+        documentVersion: pkg.version,
+        buildDate: moment().format('DD.MM.YYYY')
+      }
+    }))
+    .on('error', function (error) {
+      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('js', function () {
-    gulp.src('{js/**/*.js,node_modules/jquery/dist/jquery.min.js}')
-        .pipe(gulp.dest('build/'));
+  gulp.src('{js/**/*.js,node_modules/jquery/dist/jquery.min.js}')
+    .pipe(gulp.dest('build/'));
 });
 
 gulp.task('styles', function () {
-    gulp.src('{styles/**/*.css,node_modules/normalize.css/normalize.css}')
-        .pipe(gulp.dest('build/'));
+  gulp.src('{styles/**/*.css,node_modules/normalize.css/normalize.css}')
+    .pipe(gulp.dest('build/'));
 });
 
 gulp.task('images', function () {
-    gulp.src('images/**/*.{png,jpg,gif,svg}')
-        .pipe(gulp.dest('build/images'));
+  gulp.src('images/**/*.{png,jpg,gif,svg}')
+    .pipe(gulp.dest('build/images'));
 });
 
 gulp.task('serve', ['default'], function () {
-    var server = gls.static('build', 3000);
-    server.start();
+  var server = gls.static('build', 3000);
+  server.start();
 
-    // Restart the server when file changes
-    gulp.watch(['styles/**/*.css'], function (file) {
-        runSequence('styles', function () {
-            server.notify.apply(server, [file]);
-        });
+  // Restart the server when file changes
+  gulp.watch(['styles/**/*.css'], function (file) {
+    runSequence('styles', function () {
+      server.notify.apply(server, [file]);
     });
-    gulp.watch(['**/*.html'], function (file) {
-        runSequence('nunjucks', function () {
-            server.notify.apply(server, [file]);
-        });
+  });
+  gulp.watch(['**/*.html'], function (file) {
+    runSequence('nunjucks', function () {
+      server.notify.apply(server, [file]);
     });
-    gulp.watch(['js/**/*.js'], function (file) {
-        runSequence('js', function () {
-            server.notify.apply(server, [file]);
-        });
+  });
+  gulp.watch(['js/**/*.js'], function (file) {
+    runSequence('js', function () {
+      server.notify.apply(server, [file]);
     });
-    gulp.watch(['images/**/*.{png,jpg,gif,svg}'], function (file) {
-        runSequence('images', function () {
-            server.notify.apply(server, [file]);
-        });
+  });
+  gulp.watch(['images/**/*.{png,jpg,gif,svg}'], function (file) {
+    runSequence('images', function () {
+      server.notify.apply(server, [file]);
     });
+  });
 });
--- docs/js/open.js
+++ docs/js/open.js
@@ -3,10 +3,14 @@ const exec = require('child_process').exec;
 exec(getCommandLine() + ' ' + process.argv[2]);
 
 function getCommandLine() {
-    switch (process.platform) {
-        case 'darwin': return 'open';
-        case 'win32': return 'start';
-        case 'win64': return 'start';
-        default: return 'xdg-open';
-    }
+  switch (process.platform) {
+    case 'darwin':
+      return 'open';
+    case 'win32':
+      return 'start';
+    case 'win64':
+      return 'start';
+    default:
+      return 'xdg-open';
+  }
 }
