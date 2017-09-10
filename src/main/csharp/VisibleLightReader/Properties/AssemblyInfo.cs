17f6ea2195f46eabf22273d1dc61300bf14e3dff
Sebastian Häni <haeni.sebastian@gmail.com>
2017-09-11 00:39:39 +0200

    thermobox: activate log4net config in remaining projects

--- src/main/csharp/VisibleLightReader/Properties/AssemblyInfo.cs
+++ src/main/csharp/VisibleLightReader/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -34,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Logger configuration
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
