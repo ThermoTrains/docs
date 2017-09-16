9e00c432a8c8671526277d3d6edafdae127be0cd
Sebastian Häni <haeni.sebastian@gmail.com>
2017-09-16 16:22:41 +0200

    thermobox: new capture:abort and kill commands

--- src/main/csharp/Common/Commands.cs
+++ src/main/csharp/Common/Commands.cs
@@ -1,9 +1,11 @@
 namespace SebastianHaeni.ThermoBox.Common
 {
-  public static class Commands
-  {
-    public static readonly string CaptureStart = "cmd:capture:start";
-    public static readonly string CaptureStop = "cmd:capture:stop";
-    public static readonly string Upload = "cmd:delivery:upload";
-  }
+    public static class Commands
+    {
+        public static readonly string CaptureStart = "cmd:capture:start";
+        public static readonly string CaptureStop = "cmd:capture:stop";
+        public static readonly string CaptureAbort = "cmd:capture:abort";
+        public static readonly string Upload = "cmd:delivery:upload";
+        public static readonly string Kill = "cmd:kill";
+    }
 }
