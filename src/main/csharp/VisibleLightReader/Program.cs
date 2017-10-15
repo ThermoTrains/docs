5d74f6edaaba7138a02b6ab753b67d3c194c0802
Sebastian Häni <haeni.sebastian@gmail.com>
2017-10-15 16:30:58 +0200

    thermobox: add basler grabber sample

--- src/main/csharp/VisibleLightReader/Program.cs
+++ src/main/csharp/VisibleLightReader/Program.cs
@@ -1,9 +1,51 @@
+using System.Collections.Generic;
+using System.Reflection;
+using Basler.Pylon;
+using log4net;
+
 namespace SebastianHaeni.ThermoBox.VisibleLightReader
 {
     internal static class Program
     {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         private static void Main()
         {
+            var filter = new Dictionary<string, string> {[CameraInfoKey.FriendlyName] = "Basler acA1920-25uc"};
+            using (var camera = new Camera(filter, CameraSelectionStrategy.Unambiguous))
+            {
+                camera.CameraOpened += Configuration.AcquireContinuous;
+                camera.Open();
+                camera.StreamGrabber.Start(GrabStrategy.OneByOne, GrabLoop.ProvidedByStreamGrabber);
+
+                // Grab a number of images.
+                for (var i = 0; i < 10; ++i)
+                {
+                    // Wait for an image and then retrieve it. A timeout of 5000 ms is used.
+                    var grabResult = camera.StreamGrabber.RetrieveResult(5000, TimeoutHandling.ThrowException);
+                    using (grabResult)
+                    {
+                        // Image grabbed successfully?
+                        if (grabResult.GrabSucceeded)
+                        {
+                            // Access the image data.
+                            Log.Info($"SizeX: {grabResult.Width}");
+                            Log.Info($"SizeY: {grabResult.Height}");
+                            if (grabResult.PixelData is byte[] buffer)
+                            {
+                                Log.Info($"Gray value of first pixel: {buffer[0]}");
+                            }
+                        }
+                        else
+                        {
+                            Log.Error($"{grabResult.ErrorCode} {grabResult.ErrorDescription}");
+                        }
+                    }
+                }
+
+                // Stop grabbing.
+                camera.StreamGrabber.Stop();
+            }
         }
     }
 }
