1998650ca62bde070fd1f790f33ecc26f512b899
Sebastian Häni <haeni.sebastian@gmail.com>
2017-11-12 21:52:12 +0100

    thermobox: log version + git commit hash

--- src/main/csharp/Common/src/Component/ComponentLauncher.cs
+++ src/main/csharp/Common/src/Component/ComponentLauncher.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Reflection;
 using log4net;
 
@@ -10,6 +11,8 @@ namespace SebastianHaeni.ThermoBox.Common.Component
 
         public static void Launch(Func<ThermoBoxComponent> action)
         {
+            Init();
+
             try
             {
                 action.Invoke().Run();
@@ -19,6 +22,14 @@ namespace SebastianHaeni.ThermoBox.Common.Component
                 Log.Fatal("Uncaught exception occured", ex);
                 Environment.Exit(1);
             }
+        }
+
+        private static void Init()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            var version = fvi.FileVersion;
+            GlobalContext.Properties["version"] = version;
         }
     }
 }
